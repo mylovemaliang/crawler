466090b70b1122e8155deaad7484a5455a870b86
liang ma <mylovemaliang@sina.com>
2016-05-02 10:43:32 +0800

    test change

--- src/test/java/cn/fuyoushuo/starworld/Test.java
+++ src/test/java/cn/fuyoushuo/starworld/Test.java
@@ -4,10 +4,13 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.ResultItems;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
 import us.codecraft.webmagic.selector.Html;
+
+import java.util.Date;
 
 /**
  * Created by MALIANG on 2016/4/6.
@@ -18,19 +21,23 @@ public class Test {
 
      @org.junit.Test
      public void test1(){
-         Spider.create(new PageProcessor() {
+            long beginTime = new Date().getTime();
+            Spider spider = Spider.create(new PageProcessor() {
              @Override
              public void process(Page page) {
                  Html html = page.getHtml();
                  String ename = html.$(".star_dpage_table table tbody").xpath("//tr[4]/td[2]/text()").get();
-                 System.out.println(ename);
+                 page.putField("ename",ename);
              }
-             @Override
+            @Override
              public Site getSite() {
-                 return Site.me();
+                 return Site.me().setSleepTime(0);
              }
-         }).addUrl("http://ent.qianzhan.com/ent/star/b0110def.html").run();
-
+         });
+         Object o = spider.get("http://ent.qianzhan.com/ent/star/b0110def.html");
+         spider.close();
+         long endTime = new Date().getTime();
+         System.out.println(endTime - beginTime);
      }
 
 
